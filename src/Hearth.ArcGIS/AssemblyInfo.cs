2b5e52fec09e86daa536ab259ac2f9c3fbab65df
shaozhy <shaozhy4869@163.com>
2025-07-18 17:59:15 +0800

    updatNamespace

---
+++ src/Hearth.ArcGIS/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("https://github.com/Winemonk/Hearth.ArcGIS", "winemonk")]
+[assembly: XmlnsDefinition("https://github.com/Winemonk/Hearth.ArcGIS", "Hearth.ArcGIS")]
